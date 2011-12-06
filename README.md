db2637cb59244c0b268df8522ea6137c868e9fe4
Nikolay Kasyanov <corrmage@gmail.com>
2011-12-06 21:28:32 +0400

    initial version from emftext svn

---
+++ README.md
@@ -0,0 +1,8 @@
+This is slightly improved version of [EMFText][emftext]'s Java Properties syntax.
+PropertySet now holds EMap of KeyValuePairs and has methods for retrieving values & pairs by key.
+
+&copy; [EMFText][emftext] team -- initial version
+
+&copy; Nikolay Kayanov <corrmage@gmail.com> -- described improvement
+
+[emftext]: http://www.emftext.org/
---
+++ org.emftext.language.javaproperties.resource.properties/src/org/emftext/language/javaproperties/resource/properties/analysis/PropertiesKEYTokenResolver.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2011
+ * Software Technology Group, Dresden University of Technology
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany
+ *      - initial API and implementation
+ ******************************************************************************/
+package org.emftext.language.javaproperties.resource.properties.analysis;
+
+public class PropertiesKEYTokenResolver implements org.emftext.language.javaproperties.resource.properties.IPropertiesTokenResolver {
+	
+	private org.emftext.language.javaproperties.resource.properties.analysis.PropertiesDefaultTokenResolver defaultTokenResolver = new org.emftext.language.javaproperties.resource.properties.analysis.PropertiesDefaultTokenResolver();
+	
+	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
+		java.lang.String result = defaultTokenResolver.deResolve(value, feature, container);
+		return result;
+	}
+	
+	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.language.javaproperties.resource.properties.IPropertiesTokenResolveResult result) {
+		defaultTokenResolver.resolve(lexem, feature, result);
+	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		defaultTokenResolver.setOptions(options);
+	}
+	
+}
